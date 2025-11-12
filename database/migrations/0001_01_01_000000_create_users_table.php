b67d385258ab67e99aadc2e2f71c6ac68ada7858
Narayan Dhakal <narayandhakal443@gmail.com>
2025-11-12 17:07:56 +0545

    feat: Enhance users table migration with role_id foreign key and additional indexes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,22 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+
+
+            $table->foreignId('role_id')
+                ->constrained('roles')
+                ->onDelete('restrict');
+
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
+
+             $table->index('email');
+             $table->index('role_id');
+             $table->index('created_at');
+            $table->index('deleted_at');
+
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
