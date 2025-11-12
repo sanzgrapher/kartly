93ef86ef3069d3a21f1df4594543376150885247
Narayan Dhakal <narayandhakal443@gmail.com>
2025-11-12 17:24:21 +0545

    feat: Add Role and Seller models with migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -11,6 +12,9 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    use HasUuids;
+
+    
 
     /**
      * The attributes that are mass assignable.
@@ -19,8 +23,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'username',
         'email',
         'password',
+        'role_id',
     ];
 
     /**
@@ -45,4 +51,47 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+    
+    public function getRoleNameAttribute()
+    {
+        return $this->role?->name;
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role?->name === 'admin';
+    }
+
+    public function isSeller(): bool
+    {
+        return $this->role?->name === 'seller';
+    }
+    
+    public function hasShop(): bool
+    {
+        return $this->isSeller() && $this->seller !== null;
+    }
+
+    public function getShopAttribute()
+    {
+        return $this->seller;
+    }
+
+    public function isCustomer(): bool
+    {
+        return $this->role?->name === 'customer';
+    }
+    
+
+    
+
+
+
+
+
 }
