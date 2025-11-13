d5dcbff7fbc19b4f5343ec6470fa8f104f0cdb7c
Narayan Dhakal <narayandhakal443@gmail.com>
2025-11-13 15:45:18 +0545

    feat: Add authentication controllers for role based  login, registration, and dashboards

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+ use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    //
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $userLogin = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($userLogin)) {
+            $request->session()->regenerate();
+
+            $user = Auth::user();
+
+             if ($user->role->name === 'admin') {
+                return redirect()->route('admin.dashboard');
+            }
+
+            return redirect()->route('customer.dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided user do not match our records.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+         Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+ }
---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+ 
+class RegisterController extends Controller
+{
+    //
+    public function showRegisterForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'username' => 'required|string|max:255',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $customerRole = Role::where('name', 'customer')->firstOrFail();
+        $user = User::create([
+            'name' => $request->name,
+            'username'=>$request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role_id' => $customerRole->id,
+        ]);
+
+        Auth::login($user);
+
+
+        
+
+        return redirect()->route('customer.dashboard');
+    }
+}
