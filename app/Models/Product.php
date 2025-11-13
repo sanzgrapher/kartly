38c45baa6d3ebd9d6b11810bd6516e453c9a1e5a
Narayan Dhakal <narayandhakal443@gmail.com>
2025-11-13 21:13:53 +0545

    feat: Add Category, Payment, and Product models with relations

---
+++ app/Models/Product.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'slug',
+        'image',
+        'price',
+        'quantity',
+        'description',
+        'category_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
