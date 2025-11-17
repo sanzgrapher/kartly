9b6663ed89fcf051dae6ff65fab6fa600b646273
Narayan Dhakal <narayandhakal443@gmail.com>
2025-11-17 19:55:10 +0545

    feat: front page layout

--- resources/views/layout/public.blade.php
+++ resources/views/layout/public.blade.php
@@ -14,12 +14,15 @@
 </head>
 
 <body class="min-h-screen  bg-[#FDFDFC] text-[#1b1b18] font-sans">
- 
+    @include('layout.partials.public-header')
+
     <main class="w-full lg:max-w-6xl max-w-[1120px] mx-auto px-4 pb-16 pt-6">
+
         <h1 class="sr-only">@yield('title')</h1>
+        
         @yield('content')
     </main>
-
+    @include('layout.partials.public-footer')
 
 </body>
 
