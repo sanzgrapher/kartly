602dcae71b77b330587a445a60667e4728f1dc3a
Narayan Dhakal <narayandhakal443@gmail.com>
2025-11-19 13:27:37 +0545

    feat: iorder controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,7 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = Auth::user();
+        $orders = $user->orders()->with('payment', 'items.product')->paginate(10);
+
+        return view('customer.orders.index', compact('orders'));
+    }
+
+    public function show($id)
+    {
+        $order = Order::with('payment', 'items.product', 'user')->findOrFail($id);
+
+        if ($order->user_id !== Auth::id()) {
+            return abort(403, 'Unauthorized');
+        }
+
+        return view('customer.orders.show', compact('order'));
+    }
 }
